778c29691f37cdaca5cbcf84bd3c9c081e7b245e
Cuong Vo <cuongmv162@gmail.com>
2024-01-26 00:39:34 +0700

    Add quickscope

--- init.lua
+++ init.lua
@@ -54,11 +54,13 @@ require("lazy").setup({
 	end
 }, 
 {
+	'unblevable/quick-scope',
+},
+{
 	'stevearc/conform.nvim',
 	opts = {},
 } 
 }, opts)
-
 require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
